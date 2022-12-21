576a32f3ab84261077d319fd79fe41dac6fe1ace
mike.meloy <mike.meloy@centralreach.com>
2022-12-21 09:11:14 -0600

    Add favicon and deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const fs = require("fs"),
+  public = "./public",
+  inetpub = "C:/inetpub/wwwroot/Music/Server/public",
+  moveFiles = (files) => files.forEach(moveFile);
+
+function moveFile(fileName) {
+  fs.copyFile(`${public}/${fileName}`, `${inetpub}/${fileName}`, (err) => {
+    if (err) throw err;
+    console.log(`${fileName} was moved to ${inetpub}`);
+  });
+}
+
+fs.readdir(public, (_, files) => moveFiles(files));
