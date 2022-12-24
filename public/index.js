3b9c7ce281d2ecbd775b5e478fb33c1c8b42dd74
mike.meloy <mike.meloy@centralreach.com>
2022-12-24 15:56:26 -0600

    Added some background

--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
 import Files from "./data.js";
 
+let flip = true;
+
 const { style: variables } = document.querySelector(":root"),
   video = document.querySelector("video"),
+  canvases = document.getElementsByTagName("canvas"),
   list = document.getElementById("list"),
   createElement = (el) => document.createElement(el);
 
@@ -40,13 +43,26 @@ function createVideoLink({ root, path, ext, colors }) {
     nowPlaying.forEach((f) => f.classList.remove("marquee"));
     firstChild.classList.add("marquee");
     setColors(colors);
+    setTimeout(setBackgroundImage, 3000);
   });
 
   return btn;
 }
 
+function setBackgroundImage() {
+  const arr = Array.from(canvases),
+    [first, second] = flip ? arr : arr.reverse(),
+    ctx = first.getContext("2d");
+  
+  ctx.drawImage(video, 0, 0, first.width, first.height);
+  first.style.opacity = 1;
+  second.style.opacity = 0;
+
+  flip = !flip;
+}
+
 video.addEventListener("loadeddata", async (e) => {
-  //video.play();
+  video.play();
   try {
     await video.requestPictureInPicture();
   } catch (error) {
@@ -72,3 +88,5 @@ Files.sort(sorter).forEach((file) => {
   const btn = createVideoLink(file);
   list.appendChild(btn);
 });
+
+setInterval(setBackgroundImage, 15000);
